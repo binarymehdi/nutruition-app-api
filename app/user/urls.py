b6055d93f47aa0ce59746a828226591dddcf0ce4
binarymehdi <elmehdilaidoudi@gmail.com>
2023-07-19 16:02:47 +0100

    changes

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,10 +1,13 @@
-"""Urls mapping for user serializer"""
+"""
+Urls mapping for user serializer
+"""
 from django.urls import path
 
 from user import views
 
 
 app_name = 'user'
+
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),

c1335776875a575de383c605061ea7214f4a592d
binarymehdi <elmehdilaidoudi@gmail.com>
2023-08-15 00:00:08 +0100

    test-progress

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i$jn-z&-&x3@elnp7dwjgj6u4c@_ypgte)!+rly#jwkexlqv7j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.165']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -34,7 +34,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+
+class TagViewSet(
+        mixins.DestroyModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet):
     """Manage Tags in the database """
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -44,4 +49,3 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
-        
